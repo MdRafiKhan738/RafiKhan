c17de25400c07410fd298701675aa3928ad6b891
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-01 18:59:10 -0800

    Auto-upload

---
+++ frontend/app/Pages/works/page.jsx
@@ -0,0 +1,12 @@
+import Works from '@/app/AllPageComponents/Works'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <Works/>
+    </div>
+  )
+}
+
+export default Page
