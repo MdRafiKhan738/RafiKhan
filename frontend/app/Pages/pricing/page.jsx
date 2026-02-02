926fd5dd0255e7234cc4058ea4245c975e3dfda5
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-01 19:03:18 -0800

    Auto-upload

---
+++ frontend/app/Pages/pricing/page.jsx
@@ -0,0 +1,12 @@
+import Pricing from '@/app/AllPageComponents/Pricing'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <Pricing    />
+    </div>
+  )
+}
+
+export default Page
