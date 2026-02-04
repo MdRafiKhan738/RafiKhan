763adbe9c1811eb33fd6e208b1e0da11ea7a5532
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-03 16:38:08 -0800

    auto

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -70,6 +70,10 @@ app.use(async (req, res, next) => {
 app.get("/", (req, res) => {
   res.send("API is running 🚀")
 })
+app.get("/contact/sendmessage", (req, res) => {
+  res.json({ success: true, message: "Contact route working" })
+})
+
 
 app.use("/user", userrouter)
 app.use("/contact",contactrouter)
