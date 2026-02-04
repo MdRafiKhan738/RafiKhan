5cd14a1ee1b193190dcdf161bd4ce01aebf574f2
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-03 17:42:02 -0800

    auto

--- frontend/app/AllPageComponents/Contact.jsx
+++ frontend/app/AllPageComponents/Contact.jsx
@@ -60,7 +60,7 @@ const handlesubmit = async (e) => {
     setloading(true)
 
     const serverurl = process.env.NEXT_PUBLIC_SERVER_URL
-    const api = `${serverurl}/contact/sendmessage`
+  const api = `${serverurl.replace(/\/$/, '')}/contact/sendmessage`
 
     const response = await axios.post(api, formdata, {
       headers: { "Content-Type": "application/json" },
