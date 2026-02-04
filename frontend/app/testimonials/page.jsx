e7fd50ca075dc2a613ab1558d265ca2813db1139
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-03 20:18:08 -0800

    auto

--- frontend/app/testimonials/page.jsx
+++ frontend/app/testimonials/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Testimonials from '../AllPageComponents/Testimonials'
 
 const page = () => {
   return (
     <div>
-
+     <Testimonials />
         
     </div>
   )
