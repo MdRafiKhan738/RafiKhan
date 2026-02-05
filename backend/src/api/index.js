03cd420eaade6946dc0816f0999793fbd2793d38
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-05 17:19:48 +0600

    auto

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -17,7 +17,9 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin:[ process.env.FRONTEND_URL,
+      process.env.SECOND_FRONTEND_URL
+    ],
     methods: ["GET", "POST","Delete","PUT","PATCH"],
     credentials: true
   }
