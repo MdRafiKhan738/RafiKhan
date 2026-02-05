071b1f254d8b885f1a31c8dd828b48f69ea4bc28
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-05 13:49:34 +0600

    auto

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -44,7 +44,9 @@ io.on("connection", (socket) => {
 /* ===================== MIDDLEWARE ===================== */
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin:[ process.env.FRONTEND_URL,
+    "http://localhost:3000"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 }))
