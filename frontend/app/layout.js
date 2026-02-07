8ba9949ebc056c12a7bd98a6e7946a4bbbe6e5d5
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2026-02-07 23:11:06 +0600

    auto

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -89,7 +89,23 @@ export default function RootLayout({ children }) {
       >
         <Nav />
         {children}
-        <Chatbot />
+          <Script
+          id="tawk-chat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69877043a54a581c38e284ee/1jgsgt2ur';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
         <Footer/>
       </body>
     </html>
