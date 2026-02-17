bf6be4f8a9a6f8fc3b9b5d2158da31482afaa62d
Rafi Khan <wonderfulnatuare@gmail.com>
2026-02-17 14:57:39 +0600

    Update Projects.jsx

--- frontend/app/AllPageComponents/Projects.jsx
+++ frontend/app/AllPageComponents/Projects.jsx
@@ -53,10 +53,10 @@ const Projects = () => {
   ];
 
   return (
-    <section className="min-h-screen w-full bg-[#0f0518] text-white py-20 px-4 relative overflow-hidden flex items-center justify-center font-sans">
+    <section className="min-h-screen w-full bg-[#0f0518]  mt-15 text-white py-20 px-4 relative overflow-hidden flex items-center justify-center font-sans">
       
       {/* Dynamic Animated Background Blobs */}
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0 pointer-events-none">
+      <div className="absolute top-0 left-0 w-full h-full mt-10 overflow-hidden z-0 pointer-events-none">
         <motion.div 
           animate={{ x: [0, 100, 0], y: [0, -50, 0], scale: [1, 1.2, 1] }}
           transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
